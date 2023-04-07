181352154909d9ca497075279167bd2156df93b0
Abigail O <75042354+itsqlia@users.noreply.github.com>
2023-04-06 23:02:40 -0700

    the cooooode :)

--- src/script.js
+++ src/script.js
@@ -1,20 +1,4 @@
-// Navbar
 
-let prevScrollpos = window.pageYOffset;
-let currentScrollPos;
-window.onscroll = function () {
-    currentScrollPos = window.pageYOffset;
-    if (prevScrollpos > currentScrollPos) {
-        document.getElementById("navbar").classList.remove("hidden");
-    } else {
-        document.getElementById("navbar").classList.add("hidden");
-    }
-    prevScrollpos = currentScrollPos;
-
-    if (currentScrollPos === 0) {
-        document.getElementById("navbar").classList.remove("hidden");
-    }
-}
 
 // Slider-Collection
 function init(item) {
